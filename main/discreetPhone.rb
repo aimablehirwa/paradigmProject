008e7e5a5398393f857907a6307d989bbca180f0
aimablehirwa <aimable.hirwa@student.uclouvain.be>
2016-05-03 19:43:50 +0200

    correstion repo git

---
+++ main/discreetPhone.rb
@@ -0,0 +1,13 @@
+
+
+class DiscretePhone < Phone
+  
+  def advertiseDiscreteBeep(aPhoneCall)
+    return "discrete beep"
+  end
+  
+  def advertiseQuietly(aPhoneCall)
+    return "vibrator"
+  end
+    
+end
---
+++ main/phoneCall.rb
@@ -0,0 +1,18 @@
+
+
+class PhoneCall
+  
+  
+  def from
+    return @from  
+  end
+  
+  def from=(aString)
+    @from = aString
+  end
+  
+  def printOn(aStream)
+    #TODO
+  end
+  
+end
